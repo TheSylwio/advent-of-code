a490d9e820ed323d9db30d53150c8f7b5739f0a6
thesylwio <thesylwio@gmail.com>
2020-12-12 19:50:20 +0100

    ✨ Added Day 12 script (part 2)

--- src/Day12/index.js
+++ src/Day12/index.js
@@ -22,22 +22,22 @@ const createObjects = commands => {
   return objects;
 };
 
-const moveForward = (facing, value, east, north) => {
+const moveForward = (facing, value, shipPosition) => {
   switch (facing) {
     case 0:
-      east += value;
+      shipPosition.east += value;
       break;
     case 1:
-      north -= value;
+      shipPosition.north -= value;
       break;
     case 2:
-      east -= value;
+      shipPosition.east -= value;
       break;
     case 3:
-      north += value;
+      shipPosition.north += value;
   }
 
-  return [east, north];
+  return shipPosition;
 };
 
 const rotateShip = (direction, value, facing) => {
@@ -48,43 +48,108 @@ const rotateShip = (direction, value, facing) => {
   return (facing + rotation) % 4;
 };
 
-const moveIntoDirection = (direction, value, east, north) => {
+const moveIntoDirection = (direction, value, waypoint) => {
   switch (direction) {
     case 'N':
-      north += value;
+      waypoint.north += value;
       break;
     case 'E':
-      east += value;
+      waypoint.east += value;
       break;
     case 'S':
-      north -= value;
+      waypoint.north -= value;
       break;
     case 'W':
-      east -= value;
+      waypoint.east -= value;
   }
 
-  return [east, north];
+  return waypoint;
 };
 
-const getManhattanDistance = (east, north) => Math.abs(east) + Math.abs(north);
+const getManhattanDistance = ({ east, north }) => Math.abs(east) + Math.abs(north);
+
+const moveShipToWaypoint = (value, waypoint, ship) => {
+  const moveToEast = value * waypoint.east;
+  const moveToNorth = value * waypoint.north;
+
+  return {
+    east: ship.east + moveToEast,
+    north: ship.north + moveToNorth,
+  };
+};
+
+const moveWaypoint = (direction, value, waypoint) => {
+  const copy = { ...waypoint };
+
+  switch (direction) {
+    case 'N':
+      copy.north += value;
+      break;
+    case 'E':
+      copy.east += value;
+      break;
+    case 'S':
+      copy.north -= value;
+      break;
+    case 'W':
+      copy.east -= value;
+  }
+
+  return copy;
+};
+
+const swapCoordinates = (rotations, waypoint) => {
+  let { east, north } = { ...waypoint };
+  while (rotations > 0) {
+    [east, north] = [north, east * -1];
+    rotations--;
+  }
+
+  return { east, north };
+};
+
+const rotateWaypoint = (direction, value, waypoint) => {
+  let rotations = value / 90;
+  if (direction === 'L') rotations *= -1;
+  if (rotations < 0) rotations += 4;
+
+  return swapCoordinates(rotations % 4, waypoint);
+};
 
 const main = () => {
   const objectCommands = createObjects(commands);
-  let east = 0, north = 0, facing = 0; // Facing: 0 - EAST, 1 - SOUTH, 2 - WEST, 3 - NORTH
+  // Part 1
+  let shipPosition = { east: 0, north: 0 };
+  let facing = 0; // Facing: 0 - EAST, 1 - SOUTH, 2 - WEST, 3 - NORTH
 
   for (let { direction, value } of objectCommands) {
     if (direction === 'F') {
-      [east, north] = moveForward(facing, value, east, north);
+      shipPosition = moveForward(facing, value, shipPosition);
     }
     if (['L', 'R'].includes(direction)) {
       facing = rotateShip(direction, value, facing);
     }
     if (['N', 'E', 'S', 'W'].includes(direction)) {
-      [east, north] = moveIntoDirection(direction, value, east, north);
+      shipPosition = moveIntoDirection(direction, value, shipPosition);
     }
   }
 
-  console.log(`Manhattan distance: ${getManhattanDistance(east, north)}`);
+  // Part 2
+  let waypoint = { east: 10, north: 1 }, ship = { east: 0, north: 0 };
+
+  for (let { direction, value } of objectCommands) {
+    if (direction === 'F') {
+      ship = moveShipToWaypoint(value, waypoint, ship);
+    }
+    if (['N', 'E', 'S', 'W'].includes(direction)) {
+      waypoint = moveWaypoint(direction, value, waypoint);
+    }
+    if (['L', 'R'].includes(direction)) {
+      waypoint = rotateWaypoint(direction, value, waypoint);
+    }
+  }
+  console.log(`#1 Manhattan distance: ${getManhattanDistance(shipPosition)}`);
+  console.log(`#2 Manhattan distance: ${getManhattanDistance(ship)}`);
 };
 
 setTimeout(main, 1000);
