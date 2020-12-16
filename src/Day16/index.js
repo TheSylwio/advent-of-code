9e2fc0e1b176fd79a95d0b6a9ae11386d1a45b91
thesylwio <thesylwio@gmail.com>
2020-12-16 20:49:10 +0100

    ✨ Added Day 16 script (part 1)

---
+++ src/Day16/index.js
@@ -0,0 +1,71 @@
+const lineReader = require('line-reader');
+
+const path = './input.txt';
+let lines = [];
+
+lineReader.eachLine(path, line => {
+  lines = [...lines, line];
+});
+
+const createInstructionsObject = instructions => {
+  let object = {};
+  for (let instruction of instructions) {
+    const parts = instruction.split(': ');
+
+    const name = parts[0].replace(' ', '_');
+    const ranges = parts[1].split(' or ').map(range => {
+      const x = range.split('-');
+      return [Number(x[0]), Number(x[1])];
+    });
+
+    object = { ...object, [name]: ranges };
+  }
+
+  return object;
+};
+
+const isValueInRange = (value, array) => value >= array[0] && value <= array[1];
+
+const checkTicketErrorRate = (ticket, instructions) => {
+  const ticketValues = ticket.split(',').map(value => Number(value));
+  let errors = [];
+
+  for (let ticketValue of ticketValues) {
+    let rangesIncludesValue = [];
+    for (let ranges of Object.values(instructions)) {
+      const isInRange = isValueInRange(ticketValue, ranges[0]) || isValueInRange(ticketValue, ranges[1]);
+      rangesIncludesValue = [...rangesIncludesValue, isInRange];
+      if (isInRange) break;
+    }
+
+    if (!rangesIncludesValue.some(el => el)) {
+      errors = [...errors, ticketValue];
+    }
+  }
+
+  return errors.length > 0 ? errors.reduce((acc, curr) => acc + curr) : 0;
+};
+
+const checkGroupOfTickets = (tickets, instructions) => {
+  let results = [];
+  for (let ticket of tickets) {
+    results = [...results, checkTicketErrorRate(ticket, instructions)];
+  }
+
+  return results.reduce((acc, curr) => acc + curr);
+};
+
+
+const main = () => {
+  const instructions = lines.slice(0, 20); // 0 20 <=> 0 3
+  const ticket = lines.slice(22, 23)[0]; // 22 23 <=> 5 6
+  const nearbyTickets = lines.slice(25, lines.length); //25 LL <=> 8 LL
+
+  const instructionsObject = createInstructionsObject(instructions);
+  const ticketErrorRate = checkTicketErrorRate(ticket, instructionsObject);
+  const nearbyTicketsErrorRate = checkGroupOfTickets(nearbyTickets, instructionsObject);
+
+  console.log(ticketErrorRate, nearbyTicketsErrorRate);
+};
+
+setTimeout(main, 1000);
