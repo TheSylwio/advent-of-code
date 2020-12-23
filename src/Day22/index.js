bd7d2f17a6e843b780c654863dcc75f3a04b4aea
thesylwio <thesylwio@gmail.com>
2020-12-23 02:06:28 +0100

    ✨ Added Day 22 script (part 2)

--- src/Day22/index.js
+++ src/Day22/index.js
@@ -12,20 +12,40 @@ const getPoints = deck => {
   return deck.map(el => el * multiplier--).reduce((prev, curr) => prev + curr);
 };
 
-const main = () => {
-  const firstPlayerDeck = lines.slice(1, 26).map(el => Number(el));
-  const secondPlayerDeck = lines.slice(28, lines.length).map(el => Number(el));
+const playGame = (firstDeck, secondDeck) => {
+  const firstPlayerRounds = new Set();
+  const secondPlayerRounds = new Set();
+  let firstPlayerWonGame;
 
-  while (firstPlayerDeck.length !== 0 && secondPlayerDeck.length !== 0) {
-    const firstPlayerCard = firstPlayerDeck.shift();
-    const secondPlayerCard = secondPlayerDeck.shift();
+  while (firstDeck.length > 0 && secondDeck.length > 0) {
+    const firstPlayerRound = firstDeck.join(',');
+    const secondPlayerRound = secondDeck.join(',');
 
-    if (firstPlayerCard > secondPlayerCard) {
-      firstPlayerDeck.push(firstPlayerCard, secondPlayerCard);
+    if (firstPlayerRounds.has(firstPlayerRound) || secondPlayerRounds.has(secondPlayerRound)) return true;
+
+    const firstCard = firstDeck.shift();
+    const secondCard = secondDeck.shift();
+
+    if (firstCard <= firstDeck.length && secondCard <= secondDeck.length) {
+      firstPlayerWonGame = playGame(firstDeck.slice(0, firstCard), secondDeck.slice(0, secondCard));
     } else {
-      secondPlayerDeck.push(secondPlayerCard, firstPlayerCard);
+      firstPlayerWonGame = firstCard > secondCard;
     }
+
+    firstPlayerWonGame ? firstDeck.push(firstCard, secondCard) : secondDeck.push(secondCard, firstCard);
+
+    firstPlayerRounds.add(firstPlayerRound);
+    secondPlayerRounds.add(secondPlayerRound);
   }
+
+  return firstDeck.length > secondDeck.length;
+};
+
+const main = () => {
+  const firstPlayerDeck = lines.slice(1, 26).map(Number);
+  const secondPlayerDeck = lines.slice(28, lines.length).map(Number);
+
+  playGame(firstPlayerDeck, secondPlayerDeck);
 
   const points = firstPlayerDeck.length !== 0 ? getPoints(firstPlayerDeck) : getPoints(secondPlayerDeck);
 
